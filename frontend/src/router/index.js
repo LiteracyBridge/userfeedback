3750c9931a96b6e5276958dcf2a47abdfec10561
cliffschmidt <cliff@amplio.org>
2021-08-11 19:59:12 -0700

    added new /responses page to router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,19 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-//import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/app',
+    path: '/analyze',
     name: 'Analyze',
+    menu: true,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "analyze" */ '../views/Analyze.vue')
-  },
-  {
+  },{
+  path: '/responses',
+  name: 'Responses',
+  menu: true,
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import(/* webpackChunkName: "analyze" */ '../views/Responses.vue')
+},
+{
     path: '/login',
     name: 'Login',
     component: () =>
