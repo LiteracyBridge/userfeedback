15edb513adf560ad06ba8134448b021ee59744f6
cliffschmidt <cliff@amplio.org>
2021-08-11 19:53:29 -0700

    new Analyze.vue, which simple includes the AnalyzeComp component when it is stand-alone

---
+++ frontend/src/views/Analyze.vue
@@ -0,0 +1,13 @@
+<template>
+  <analyze-comp/>
+</template>
+<script>
+import AnalyzeComp from '../components/AnalyzeComp.vue';
+
+export default {
+  name:"Analyze",
+  components: {
+    AnalyzeComp
+  }
+}
+</script>
